70ec70a15ba8cf500f42a69422df0fd6a9e257ec
Formacion <masalinas>
2018-06-26 19:05:58 +0200

    last update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductComponent }   from './product/product.component';
+import { OrderComponent }   from './order/order.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'product', component: ProductComponent },
+  { path: 'order', component: OrderComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SDKBrowserModule } from './shared/sdk/index';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import es from '@angular/common/locales/es';
 import { NgZorroAntdModule, NZ_I18N, es_ES } from 'ng-zorro-antd';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SDKBrowserModule } from './shared/sdk/index';
+import { ProductComponent } from './product/product.component';
 import { OrderComponent } from './order/order.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import es from '@angular/common/locales/es';
 
 registerLocaleData(es);
 
 @NgModule({
-  declarations: [AppComponent, OrderComponent],
+  declarations: [
+    AppComponent, 
+    ProductComponent, 
+    OrderComponent
+  ],
   imports: [
     BrowserModule, 
     CommonModule,
@@ -22,7 +28,8 @@ registerLocaleData(es);
     SDKBrowserModule.forRoot(),
     NgZorroAntdModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [{ provide: NZ_I18N, useValue: es_ES }],
   bootstrap: [AppComponent]
