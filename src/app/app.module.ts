405185af15f7268f20f7889cbe01ad3c21be0443
Formacion <masalinas>
2018-06-26 21:19:22 +0200

    last update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule, registerLocaleData } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import es from '@angular/common/locales/es';
 import { NgZorroAntdModule, NZ_I18N, es_ES } from 'ng-zorro-antd';
 
 import { AppComponent } from './app.component';
@@ -13,6 +12,7 @@ import { SDKBrowserModule } from './shared/sdk/index';
 import { ProductComponent } from './product/product.component';
 import { OrderComponent } from './order/order.component';
 
+import es from '@angular/common/locales/es';
 registerLocaleData(es);
 
 @NgModule({
@@ -29,6 +29,7 @@ registerLocaleData(es);
     NgZorroAntdModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [{ provide: NZ_I18N, useValue: es_ES }],
