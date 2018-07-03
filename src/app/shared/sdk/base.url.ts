816a7ff251bf6bf908dace722f5ac66ee450ef97
Andres Carmona <valdepeace@gmail.com>
2018-07-03 16:42:50 +0200

    fix table

--- src/app/shared/sdk/base.url.ts
+++ src/app/shared/sdk/base.url.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = 'http://127.0.0.1:4000';
+//export const BASE_URL = 'https://api.eu.apiconnect.ibmcloud.com/masalinasgancedogmailcom-dev/production/api/orders';
 export const API_VERSION = 'api';
